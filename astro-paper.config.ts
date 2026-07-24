882997588b5ef60db62ffb2b71f12e3e368a81a8
unknown <bagua010190@gmail.com>
2026-07-24 11:27:21 +0800

    init: astro-paper with GitHub Pages deployment

---
+++ astro-paper.config.ts
@@ -0,0 +1,38 @@
+import { defineAstroPaperConfig } from "./src/types/config";
+
+export default defineAstroPaperConfig({
+  site: {
+    url: "https://ray-10190.github.io/rays-digital-notes/",
+    title: "Ray's Digital Notes",
+    description: "Ray's digital notes blog.",
+    author: "Ray",
+    profile: "https://github.com/Ray-10190",
+    ogImage: "default-og.jpg",
+    lang: "en",
+    timezone: "Asia/Bangkok",
+    dir: "ltr",
+  },
+  posts: {
+    perPage: 4,
+    perIndex: 4,
+    scheduledPostMargin: 15 * 60 * 1000,
+  },
+  features: {
+    lightAndDarkMode: true,
+    dynamicOgImage: true,
+    showArchives: true,
+    showBackButton: true,
+    editPost: {
+      enabled: true,
+      url: "https://github.com/Ray-10190/rays-digital-notes/edit/main/",
+    },
+    search: "pagefind",
+  },
+  socials: [
+    { name: "github",   url: "https://github.com/Ray-10190" },
+    { name: "mail",     url: "mailto:yourmail@gmail.com" },
+  ],
+  shareLinks: [
+    { name: "mail",     url: "mailto:?subject=See%20this%20post&body=" },
+  ],
+});
